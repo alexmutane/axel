e59dee08d8d24ffea47986400c9b949172d7d94a
AlexMutane <alexmutane@live.com>
2019-04-26 17:21:43 +0200

    projecto inicializado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,51 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  mostrarCategorias: boolean;
+  mostrarSubcategorias: boolean;
 
+  subcategorias = {
+    apontamentos: false,
+    ideias: false,
+    outros: false
+  }
+
+  constructor(
+    public navCtrl: NavController
+  ) {
+    this.mostrarCategorias = true;
+  }
+
+  abrirSubcategorias(categoria){
+    this.mostrarCategorias = false;
+    this.mostrarSubcategorias = true;
+
+    switch(categoria){
+      case 'apontamentos':  { 
+        this.subcategorias.apontamentos = true;
+        this.subcategorias.ideias = false; 
+        this.subcategorias.outros = false;
+        break;
+      }
+      case 'ideias':  { 
+        this.subcategorias.apontamentos = false;
+        this.subcategorias.ideias = true; 
+        this.subcategorias.outros = false;
+        break; 
+      }
+      case 'outros':  { 
+        this.subcategorias.apontamentos = false;
+        this.subcategorias.ideias = false; 
+        this.subcategorias.outros = true;
+        break; 
+      }
+      default: this.subcategorias.apontamentos = true; 
+    }
+  }
+
+  listarCategorias(){
+    this.mostrarCategorias = true;
+    this.mostrarSubcategorias = false;
   }
 
 }
